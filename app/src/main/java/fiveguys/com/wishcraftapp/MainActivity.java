ab98768bfa7dc4c9d6ea385f2feae5a72e780dbb
Dan Menjivar <daniel.quintanamenjivar882@myci.csuci.edu>
2019-03-07 10:24:20 -0800

    removed extraneous files

--- app/src/main/java/fiveguys/com/wishcraftapp/MainActivity.java
+++ app/src/main/java/fiveguys/com/wishcraftapp/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_logindependent);
     }
 
 
