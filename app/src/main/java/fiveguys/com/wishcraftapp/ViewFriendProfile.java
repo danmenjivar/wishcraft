c2670c32fefe1283ff8b553407eb2108e1ecd952
ConnorStephens7 <45470138+ConnorStephens7@users.noreply.github.com>
2019-03-09 15:34:40 -0800

    Revert "Dan"

--- app/src/main/java/fiveguys/com/wishcraftapp/ViewFriendProfile.java
+++
@@ -1,6 +0,0 @@
-package fiveguys.com.wishcraftapp;
-
-import android.app.Activity;
-
-public class ViewFriendProfile extends Activity {
-}
---
+++ app/src/main/java/fiveguys/com/wishcraftapp/view_friend_profile.java
@@ -0,0 +1,13 @@
+package fiveguys.com.wishcraftapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class view_friend_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_friend_profile);
+    }
+}
