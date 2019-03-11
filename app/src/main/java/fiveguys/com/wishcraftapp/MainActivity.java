cdc3f1060100826939f1647ef26c7b894ce01f6e
hiddenred <cody.roelofson783@myci.csuci.edu>
2019-03-11 16:17:54 -0700

    More updated version of cleaned search

--- app/src/main/java/fiveguys/com/wishcraftapp/MainActivity.java
+++ app/src/main/java/fiveguys/com/wishcraftapp/MainActivity.java
@@ -1,7 +1,9 @@
 package fiveguys.com.wishcraftapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void user1click(View view) {
+        Intent intent = new Intent(this, ActivityName.class);
+        startActivity(intent);
+    }
+
+
 }
