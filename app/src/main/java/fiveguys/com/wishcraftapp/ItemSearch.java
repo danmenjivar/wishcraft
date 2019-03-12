18ef69eb4a06ee5c28545e6edf669a4fbc100e68
hiddenred <cody.roelofson783@myci.csuci.edu>
2019-03-12 14:49:50 -0700

    More updated version of cleaned search

--- app/src/main/java/fiveguys/com/wishcraftapp/ItemSearch.java
+++ app/src/main/java/fiveguys/com/wishcraftapp/ItemSearch.java
@@ -4,8 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class ItemSearch extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +15,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void user1click(View view) {
-        Intent intent = new Intent(this, ActivityName.class);
+       // Intent intent = new Intent(this, ItemSearch.class);
+        Toast.makeText(this, "Added Item to list", Toast.LENGTH_SHORT).show();
+       // startActivity(intent);
+    }
+
+    public void toMyProfile(View view) {
+        Intent intent = new Intent(this, MyProfile.class);
+
         startActivity(intent);
     }
 
