f79d4681ce9d7479bf5b1ad562760a4cfed9ea46
Dan Menjivar <daniel.quintanamenjivar882@myci.csuci.edu>
2019-03-25 17:20:03 -0700

    firebase: can login users

--- app/src/main/java/fiveguys/com/wishcraftapp/Login.java
+++ app/src/main/java/fiveguys/com/wishcraftapp/Login.java
@@ -1,32 +1,44 @@
 package fiveguys.com.wishcraftapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
-//    private FirebaseAuth mAuth;
+    private FirebaseAuth mAuth; //firebase user login authorization
+
+    private EditText loginEmailEditText;
+    private EditText passwordEditText;
+    private final String TAG = "LoginScreen";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FirebaseApp.initializeApp(this);
+        FirebaseApp.initializeApp(this); //must be called for other fb methods to work
         setContentView(R.layout.activity_login);
-//        FirebaseApp.initializeApp(this); //onCreate method used to connect to Firebase
-//        mAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance(); //grab the shared instance
+        this.loginEmailEditText = (EditText) findViewById(R.id.login_email);
+        this.passwordEditText = (EditText) findViewById(R.id.login_password);
     }
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-//        FirebaseUser currentUser = mAuth.getCurrentUser();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //TODO: go to User's profile
 //        updateUI(currentUser);
     }
 
@@ -42,7 +54,31 @@ public class Login extends AppCompatActivity {
     }
 
     public void loginButton(View view) {
-        Intent loginIntent = new Intent(this, Feed.class);
-        startActivity(loginIntent);
+        String email = loginEmailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //TODO send the user to their feed
+                            //updateUI(user);
+                            Intent loginIntent = new Intent(Login.this, Feed.class);
+                            startActivity(loginIntent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+
+
+
     }
 }
