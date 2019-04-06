cf242a34fc7b38f779b9b0caf1a9addc323adc8d
Dan Menjivar <daniel.quintanamenjivar882@myci.csuci.edu>
2019-04-06 16:16:45 -0700

    Added change email and username functionality

--- app/src/main/java/fiveguys/com/wishcraftapp/Settings.java
+++ app/src/main/java/fiveguys/com/wishcraftapp/Settings.java
@@ -9,8 +9,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -26,7 +24,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -34,20 +31,21 @@ import com.google.firebase.storage.StorageReference;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Map;
 
 public class Settings extends Activity {
 
     private final String TAG = "Settings";
     private EditText usernameText;
     private FirebaseUser mUser;
-    private DatabaseReference mUsersDatabase;
+    private DatabaseReference mDatabase;
     private StorageReference mStorageRef;
     private FirebaseAuth mAuth;
     public static final int GET_FROM_GALLERY = 20;
     private ImageView profilePicture;
     private EditText emailText;
     private User loggedInUser;
-    private Button saveChangesButton;
+    private String userKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,25 +58,26 @@ public class Settings extends Activity {
         usernameText = findViewById(R.id.username_change);
         loggedInUser = new User("", "");
         String email = mUser.getEmail(); //grab email to locate in database
-
         mStorageRef = FirebaseStorage.getInstance().getReference();
-        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("users");///connect to fb database
+        mDatabase = FirebaseDatabase.getInstance().getReference();///connect to fb database
         fetchUser(email);
     }
 
     //Fetches user data to display in settings
-    private void fetchUser(String email){
-        mUsersDatabase.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
+    private void fetchUser(String email) {
+        mDatabase.child("users").orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-               if (dataSnapshot.hasChildren()){
-                   User user = dataSnapshot.getChildren().iterator().next().getValue(User.class);
-                   //Log.d(TAG, user.username + " " + user.email);
-                   emailText.setText(user.email);
-                   loggedInUser.setEmail(user.email);
-                   usernameText.setText(user.username);
-                   loggedInUser.setUsername(user.username);
-               }
+                if (dataSnapshot.hasChildren()) {
+                    DataSnapshot userData = dataSnapshot.getChildren().iterator().next();
+                    userKey = userData.getKey();//used when changing username and email
+                    User user = userData.getValue(User.class);
+                    //Log.d(TAG, user.username + " " + user.email);
+                    emailText.setText(user.email);
+                    loggedInUser.setEmail(user.email);
+                    usernameText.setText(user.username);
+                    loggedInUser.setUsername(user.username);
+                }
             }
 
             @Override
@@ -89,14 +88,14 @@ public class Settings extends Activity {
     }
 
     //Method will straight up just delete that user from being authenticated
-    public void onDeleteButtonClick(View view){
-        //Todo: add a dialogue box to confirm
+    public void onDeleteButtonClick(View view) {
+        //Todo: add a dialogue box to confirm S2
 
         mUser.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()){
-                    Log.d(TAG, "delete succesful");
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "delete successful");
                 } else {
                     Log.d(TAG, "user could not be deleted");
                 }
@@ -106,24 +105,24 @@ public class Settings extends Activity {
     }
 
     //Method executes when user types new Password
-    public void changePasswordOnClick(View view){
+    public void changePasswordOnClick(View view) {
 
         EditText newPassword = findViewById(R.id.PasswordEntry);
         EditText newPasswordConfirm = findViewById(R.id.PasswordEntryConfirm);
 
-        if (arePasswordsEqualAndValid(newPassword, newPasswordConfirm)){
+        if (arePasswordsEqualAndValid(newPassword, newPasswordConfirm)) {
             changePassword(newPassword.getText().toString());
         }
     }
 
     //Method updates firebase auth with new password for currently signed in user
-    private void changePassword(String newPassword){
+    private void changePassword(String newPassword) {
         mUser.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()){
+                if (task.isSuccessful()) {
                     Log.d(TAG, "password updated");
-                    Toast.makeText(Settings.this, "Changes saved.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Settings.this, "Password updated", Toast.LENGTH_SHORT).show();
                 } else {
                     Log.d(TAG, "error, password not updated", task.getException());
                 }
@@ -132,53 +131,104 @@ public class Settings extends Activity {
     }
 
 
-    private void changeUsername(String email, String newUsername){
-        mUsersDatabase.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
+    //Method updates username in firebase realtime database
+    private void updateDatabaseUsername(final String newUsername) {
+        loggedInUser.setUsername(newUsername);
+        Map<String, Object> userValues = loggedInUser.toMap();
+        Log.d(TAG, String.format("User key entered was %s", userKey));
+        mDatabase.child("users/" + userKey).updateChildren(userValues).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "username change was successful");
+                    Toast.makeText(Settings.this, String.format("Username changed to %s", newUsername), Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.d(TAG, "username change failed", task.getException());
+                }
+
+            }
+        });
+    }
+
+    /**
+     * When user clicks to save changes to their bio or username
+     *
+     * @param view
+     */
+    public void saveChangesButton(View view) {
+
+        String username = usernameText.getText().toString();
+        String email = emailText.getText().toString();
+
+        boolean hasUserChangedUsernameField = !username.equals(loggedInUser.username);
+        boolean hasUserChangedEmailField = !email.equals(loggedInUser.email);
+
+        if (hasUserChangedUsernameField) {
+            updateDatabaseUsername(username);
+        }
+
+        if (hasUserChangedEmailField) {
+            updateEmailAuth(email);
+        }
+
+    }
+
+    //Method updates username in firebase realtime database
+    private void updateDatabaseEmail(final String newEmail) {
+        //First update the wishlist table
+        mDatabase.child("userWishlist").orderByChild("email").equalTo(loggedInUser.email).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()){
-                    for (DataSnapshot ds: dataSnapshot.getChildren()){
-                        String keyId = ds.getKey();
-                    }
-
-                    User user = dataSnapshot.getChildren().iterator().next().getValue(User.class);
-                    //Log.d(TAG, user.username + " " + user.email);
+                if (dataSnapshot.hasChildren()) {
+                    DataSnapshot ds = dataSnapshot.getChildren().iterator().next();
+                    String key = ds.getKey();
+                    mDatabase.child("userWishlist").child(key).child("email").setValue(newEmail);
                 }
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                //couldn't read anything
+                //exists for compilation, does nothing
+            }
+        });
+
+        //then update, the users table
+        loggedInUser.setEmail(newEmail);
+        Map<String, Object> userValues = loggedInUser.toMap();
+        Log.d(TAG, String.format("User key entered was %s", userKey));
+        mDatabase.child("users/" + userKey).updateChildren(userValues).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "email change was successful");
+                } else {
+                    Log.d(TAG, "email change failed", task.getException());
+                }
+
+            }
+        });
+        Toast.makeText(this, String.format("%s saved as email", newEmail)
+                , Toast.LENGTH_SHORT).show();
+    }
+
+    //Updates email in the user login authentication and then updates in the r.t.d.b.
+    private void updateEmailAuth(final String email) {
+
+        mUser.updateEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, String.format("Email updated successfully to %s in FBAuth", email));
+                    updateDatabaseEmail(email);
+                } else {
+                    Toast.makeText(Settings.this, email + " is already used", Toast.LENGTH_SHORT).show();
+                    //Log.d(TAG,"failed to update email in FBAuth", task.getException());
+                }
             }
         });
 
     }
 
-    /**
-     * When user clicks to save changes to their bio or username
-     * @param view
-     */
-    public void saveChangesButton(View view) {
-
-        boolean hasUserChangedUsernameField = !usernameText.getText().toString().equals(loggedInUser.username);
-        boolean hasUserChangedEmailField = !emailText.getText().toString().equals(loggedInUser.email);
-
-        if (hasUserChangedUsernameField){
-
-        }
-
-
-
-        if (hasUserChangedEmailField || hasUserChangedUsernameField){//user has changed either password or email field
-
-        }
-
-
-
-
-    }
-
-   //Signs out user when they click the log out button system wide
+    //Signs out user when they click the log out button system wide
     public void backToLogin(View view) {
         FirebaseAuth.getInstance().signOut(); //sign out user system-wide
         Intent createAccountIntent = new Intent(this, Login.class);
@@ -191,34 +241,32 @@ public class Settings extends Activity {
     public void onClickEditPic(View v) {
         Intent galleryImageGrab = new Intent(Intent.ACTION_PICK);
         File picDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        String picDirPath =  picDirectory.getPath();
+        String picDirPath = picDirectory.getPath();
         Uri picData = Uri.parse(picDirPath);
-        galleryImageGrab.setDataAndType(picData,"image/*");
+        galleryImageGrab.setDataAndType(picData, "image/*");
         startActivityForResult(galleryImageGrab, GET_FROM_GALLERY);
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(resultCode == RESULT_OK){
-            if(requestCode == GET_FROM_GALLERY){
+        if (resultCode == RESULT_OK) {
+            if (requestCode == GET_FROM_GALLERY) {
                 //successfully heard from image gallery if here
                 String userID = mUser.getUid();
                 Uri imageUri = data.getData();
                 InputStream imgStream;
-                StorageReference storageReference = mStorageRef.child("images/ProfilePics/"+ userID+".jpg");
-                //Log.d("danny", "username");
-
+                StorageReference storageReference = mStorageRef.child("images/ProfilePics/" + userID + ".jpg");
                 try {
                     imgStream = getContentResolver().openInputStream(imageUri);
                     Bitmap bitmap = BitmapFactory.decodeStream(imgStream);
-                    if(bitmap != null) {
+                    if (bitmap != null) {
                         profilePicture.setImageBitmap(bitmap);
                     }
                     storageReference.putFile(imageUri);
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
-                    Toast.makeText(this,"Unable to Open Image",Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, "Unable to Open Image", Toast.LENGTH_LONG).show();
                 }
             }
 
@@ -232,18 +280,18 @@ public class Settings extends Activity {
     }
 
     //Checks if both passwords match and the user entered are valid
-    private boolean arePasswordsEqualAndValid(EditText password1, EditText password2){
+    private boolean arePasswordsEqualAndValid(EditText password1, EditText password2) {
         String newPassword = password1.getText().toString();
         String newPasswordConfirm = password2.getText().toString();
 
         boolean arePasswordsValid = isValidPassword(newPassword) && isValidPassword(newPasswordConfirm);
 
-        if (!arePasswordsValid){
+        if (!arePasswordsValid) {
             Toast.makeText(this, "Too short", Toast.LENGTH_SHORT).show();
             return false;
         }
 
-        if (!newPassword.equals(newPasswordConfirm)){
+        if (!newPassword.equals(newPasswordConfirm)) {
             Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
             return false;
         }
