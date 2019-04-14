f8ce26b0dc332e1ad83f0afde4d2540f4a7f5898
riad <riadelkhoury@gmail.com>
2019-04-14 14:23:56 -0700

    fixed fragment layout issue, should now center all structs in the fragment xmls. still need to implement full screen implementation

--- app/src/main/java/fiveguys/com/wishcraftapp/HomeFragment.java
+++ app/src/main/java/fiveguys/com/wishcraftapp/HomeFragment.java
@@ -6,7 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +22,8 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
 }
