ba41cba0480452b9bdfa361bb1a4081b49c2660e
Dan Menjivar <Daniel.quintanamenjivar882@myci.csuci.edu>
2019-04-17 14:22:51 -0700

    attempting Ali

--- app/src/main/java/fiveguys/com/wishcraftapp/ItemSearchFragment.java
+++ app/src/main/java/fiveguys/com/wishcraftapp/ItemSearchFragment.java
@@ -1,29 +1,41 @@
 package fiveguys.com.wishcraftapp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ItemSearchFragment extends Fragment {
 
-
     public ItemSearchFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //TextView textView = new TextView(getActivity());
-        //textView.setText(R.string.hello_blank_fragment);
-        return inflater.inflate(R.layout.itemsearch, container, false);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_item_search, container, false);
     }
 }
