8483294ddd17ef3cfa1ca0aa8cb4f2b8fd4bc0a0
Dan Menjivar <Daniel.quintanamenjivar882@myci.csuci.edu>
2019-04-18 16:44:05 -0700

    1) Reconnected Login Activity to Main Activity for Testing with Firebase 2) Added a manual entry option for adding items to a wishlist thru a dialog box 3) Added internal support for adding items to a wishlist onto Firebase 4) Added verification for search queries 5) Added Item class 6) Added AddItemDialog Class 7) Added to Gradle Currency TextEdit library

--- app/src/main/java/fiveguys/com/wishcraftapp/AliTest.java
+++ app/src/main/java/fiveguys/com/wishcraftapp/AliTest.java
@@ -1,40 +1,60 @@
 package fiveguys.com.wishcraftapp;
 
-import android.preference.PreferenceActivity;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.gson.JsonArray;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
 
-public class AliTest extends AppCompatActivity {
+public class AliTest extends AppCompatActivity implements AddItemDialog.AddItemDialogListener {
 
+    private final static String NAME = "item_name";
+    private final static String PRICE = "item_price";
+    private final static String LINK = "item_link";
     private static final String DEBUG_TAG = "Ali";
     private final String apiAccessKey = "YNZBUIVFBSOPLNSO";
+    private FirebaseAuth fbauth;
+    private String userEmail;
+    private String fbUserKey;
+    private EditText searchQuery;
+    private Button searchButton;
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ali_test);
+        fbauth = FirebaseAuth.getInstance();
+        userEmail = fbauth.getCurrentUser().getEmail();
+        searchQuery = findViewById(R.id.search_query_edittext);
+        searchButton = findViewById(R.id.search_button);
+        searchQuery.addTextChangedListener(onSearchQueryEntered);
     }
 
     private void searchAliExpress(String itemSearchQuery) {
         String url = "https://api.aliseeks.com/v1/search";
-
 
         JsonObject request = new JsonObject();
         request.addProperty("text", itemSearchQuery);
         request.addProperty("sort", "BEST_MATCH");
         request.addProperty("currency", "USD");
 
-
         Log.d(DEBUG_TAG, "The POST request is: " + request.toString());
 
         Ion.with(this)
@@ -46,30 +66,34 @@ public class AliTest extends AppCompatActivity {
                     @Override
                     public void onCompleted(Exception e, JsonObject result) {
                         Log.d(DEBUG_TAG, "The JSON result is " + result.toString());
+                        processJsonResponse(result);
                     }
                 });
 
     }
 
     public void onSearchButtonClick(View view) {
-        EditText userInput = findViewById(R.id.search_query_edittext);
-        String userQuery = userInput.getText().toString();
+        String userQuery = searchQuery.getText().toString();
         searchAliExpress(userQuery);
-        //searchAliBestSelling();
     }
 
+    public void onAddItemManuallyClick(View view) {
+        showAddItemDialog();
+    }
+
+    private void showAddItemDialog() {
+        AddItemDialog addItemDialog = new AddItemDialog();
+        addItemDialog.show(getSupportFragmentManager(), "addItemDialog");
+    }
 
     private void searchAliBestSelling() {
 
-
         String url = "https://api.aliseeks.com/v1/search";
-
 
         JsonObject request = new JsonObject();
         request.addProperty("sort", "MONTHLY_TRANSACTION_RATE");
         request.addProperty("currency", "USD");
         request.addProperty("category", 100003070);
-
 
         Log.d(DEBUG_TAG, "The POST request is: " + request.toString());
 
@@ -86,9 +110,70 @@ public class AliTest extends AppCompatActivity {
                 });
     }
 
-    private void processJson(JsonObject jsonObject) {
-        Log.d(DEBUG_TAG, "The JSON result is " + String.format("%s", jsonObject.toString()));
+    private void processJsonResponse(JsonObject jsonObject) {
+        //TODO
+
+    }
+
+    @Override
+    public void applyItemData(String itemName, Double itemPrice, String itemLink) {
+        Item itemToAdd = null;
+
+        try {
+            itemToAdd = new Item(itemName, itemPrice, itemLink);
+            addItemToDatabase(itemToAdd);
+        } catch (Exception e) {
+            Toast.makeText(this, "Can't add item with missing attributes!", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
+    private void addItemToDatabase(final Item item) {
+        final DatabaseReference database = FirebaseDatabase.getInstance().getReference("userWishlist");
+        database.orderByChild("email").equalTo(userEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChildren()) {
+                    fbUserKey = dataSnapshot.getChildren().iterator().next().getKey();
+                    Log.d(DEBUG_TAG + ": Firebase", "User key to write to fb is: " + fbUserKey);
+                    DatabaseReference newItem = database.child(fbUserKey + "/wishlist").push();
+                    newItem.child(NAME).setValue(item.getItemName());
+                    newItem.child(PRICE).setValue(item.getItemPrice());
+                    newItem.child(LINK).setValue(item.getItemLink());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //needs to be empty to compile
+            }
+        });
+
+    }
+
+    private TextWatcher onSearchQueryEntered = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String userInput = searchQuery.getText().toString();
+            if (userInput.isEmpty() || userInput.trim().length() == 0){//empty or whitespace
+                searchButton.setEnabled(false);
+                searchButton.setBackgroundColor(getResources().getColor(R.color.disable_grey));
+            } else {
+                searchButton.setEnabled(true);
+                searchButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            //empty but neccesary to compile
+        }
+    };
 }
+
+
