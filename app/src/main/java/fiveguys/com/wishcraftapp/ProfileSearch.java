9d2aa7d595c3e2ec796c1d9fb627ac99e53e4a6e
Diep <jason.diep368@SIE-1222-02.local>
2019-04-22 16:16:37 -0700

    working on hardcoding 'Tom from Myspace' to friendslist

--- app/src/main/java/fiveguys/com/wishcraftapp/ProfileSearch.java
+++ app/src/main/java/fiveguys/com/wishcraftapp/ProfileSearch.java
@@ -5,11 +5,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ProfileSearch extends AppCompatActivity {
+
+    private DatabaseReference friendsListdb;
+    private String currentUserEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_search);
+        friendsListdb = FirebaseDatabase.getInstance().getReference("userFriendslist");
+        currentUserEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
     }
 
 
@@ -17,6 +27,14 @@ public class ProfileSearch extends AppCompatActivity {
         Intent addItem = new Intent(this,ViewFriendProfile.class);
         startActivity(addItem);
     }
+
+    //TODO pricate void and hardcode a new friend
+
+    private void addFriendToFriendList(String nameOfFriend){
+        friendsListdb.orderByChild("email").equalTo(currentUserEmail);
+    }
+
+
 
 
 }
