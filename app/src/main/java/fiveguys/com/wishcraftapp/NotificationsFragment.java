d2e333c98173a143def2cfcbaff96a6afe021f13
Jason Diep <jason.diep368@myci.csuci.edu>
2019-04-24 07:55:26 -0700

    Could not find method in parent or ancestor context

--- app/src/main/java/fiveguys/com/wishcraftapp/NotificationsFragment.java
+++ app/src/main/java/fiveguys/com/wishcraftapp/NotificationsFragment.java
@@ -7,12 +7,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NotificationsFragment extends Fragment {
 
+    private DatabaseReference friendsListdb;
+    private String currentUserEmail;
 
     public NotificationsFragment() {
         // Required empty public constructor
@@ -22,8 +27,21 @@ public class NotificationsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_profile_search);
+        friendsListdb = FirebaseDatabase.getInstance().getReference("userFriendslist");
+        currentUserEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+        //return inflater.inflate(R.layout.fragment_notifications, container, false);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_profile_search, container, false);
     }
 
+    //TODO pricate void and hardcode a new friend
+
+    private void addFriendToFriendList(String nameOfFriend){
+        friendsListdb.orderByChild("email").equalTo(currentUserEmail);
+    }
 }
--- app/src/main/java/fiveguys/com/wishcraftapp/ProfileSearch.java
+++ app/src/main/java/fiveguys/com/wishcraftapp/ProfileSearch.java
@@ -8,6 +8,30 @@ import android.view.View;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import android.media.*;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.signature.StringSignature;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ProfileSearch extends AppCompatActivity {
 
@@ -28,10 +52,20 @@ public class ProfileSearch extends AppCompatActivity {
         startActivity(addItem);
     }
 
-    //TODO pricate void and hardcode a new friend
+    //TODO private void and hardcode a new friend
 
-    private void addFriendToFriendList(String nameOfFriend){
+    public void addFriendToFriendListButton(View view){
         friendsListdb.orderByChild("email").equalTo(currentUserEmail);
+//        friendsListdb.child("friendslist").setValue("Tom from Myspace");
+//        friendsListdb.push();
+
+        DatabaseReference table = friendsListdb.child("friendlist");
+        DatabaseReference newFriend = table.push();
+        newFriend.child("friends").setValue("Tom from Myspace");
+
+        Toast.makeText(this, "Added Friend to list", Toast.LENGTH_SHORT).show();
+
+
     }
 
 
