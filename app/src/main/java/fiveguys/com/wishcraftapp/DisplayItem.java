19cd8b7fadf9250bfa7a2c7ded0065694126ccaf
Dan Menjivar <Daniel.quintanamenjivar882@myci.csuci.edu>
2019-04-29 13:21:40 -0700

    Can fetch items off firebase database and place them into an arrayList.

---
+++ app/src/main/java/fiveguys/com/wishcraftapp/DisplayItem.java
@@ -0,0 +1,78 @@
+package fiveguys.com.wishcraftapp;
+
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DisplayItem {
+
+    private String item_name;
+    private double item_price;
+    private String item_link;
+    private String item_image_url;
+
+    public DisplayItem() {
+        //empty on purpose
+    }
+
+    public DisplayItem(String itemName, double itemPrice, String itemLink, String imageUrl) {
+        this.item_name = itemName;
+        this.item_price = itemPrice;
+        this.item_link = itemLink;
+        this.item_image_url = imageUrl;
+    }
+
+    public String getItemName() {
+        return item_name;
+    }
+
+    public double getItemPrice() {
+        return item_price;
+    }
+
+    public String getImageUrl() {
+        return item_image_url;
+    }
+
+    public String getItemLink() {
+        return item_link;
+    }
+
+    public void setItemName(String itemName) {
+        this.item_name = itemName;
+    }
+
+    public void setItemPrice(double itemPrice) {
+        this.item_price = itemPrice;
+    }
+
+    public void setItemLink(String itemLink) {
+        this.item_link = itemLink;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.item_image_url = imageUrl;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap(){
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("item_link", this.item_link);
+        result.put("item_name", this.item_name);
+        result.put("item_price", this.item_price);
+        result.put("item_image_url", this.item_image_url);
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "AliItem{" +
+                "itemName='" + item_name + '\'' +
+                ", itemPrice=" + item_price +
+                ", itemLink='" + item_link + '\'' +
+                ", imageUrl='" + item_image_url + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/fiveguys/com/wishcraftapp/MyProfile.java
+++ app/src/main/java/fiveguys/com/wishcraftapp/MyProfile.java
@@ -1,49 +1,25 @@
 package fiveguys.com.wishcraftapp;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.media.*;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.Priority;
-import com.bumptech.glide.load.data.DataFetcher;
-import com.bumptech.glide.load.model.stream.StreamModelLoader;
 import com.bumptech.glide.signature.StringSignature;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
-import com.google.firebase.FirebaseError;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -52,24 +28,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.annotations.NotNull;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 //import com.bumptech.glide.annotation.GlideModule;
 //import com.bumptech.glide.module.AppGlideModule;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Map;
 
 public class MyProfile extends AppCompatActivity {
 
     private static final String TAG = "MyProfile";
-    public static final int GET_FROM_GALLERY =20;
+    public static final int GET_FROM_GALLERY = 20;
     private ImageView profilePicture;
     private DatabaseReference mDatabase;
     private StorageReference mStorageRef;
@@ -80,6 +51,7 @@ public class MyProfile extends AppCompatActivity {
     private EditText bioEditText;
     private User loggedInUser;
     private Button changeBioButton;
+    private ArrayList<DisplayItem> wishListArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,6 +70,8 @@ public class MyProfile extends AppCompatActivity {
         bioEditText.addTextChangedListener(isBioChanged);
         changeBioButton = findViewById(R.id.change_bio_button);
         setUserBio();
+        findUserWishlist();
+        wishListArrayList = new ArrayList<>();
 
     }
 
@@ -178,6 +152,64 @@ public class MyProfile extends AppCompatActivity {
                 } else {
                     Log.d(TAG, "username change failed", task.getException());
                 }
+
+            }
+        });
+    }
+
+    private void findUserWishlist(){
+        mDatabase.child("userWishlist").orderByChild("email").equalTo(mUser.getEmail())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChildren()){
+                    DataSnapshot user = dataSnapshot.getChildren().iterator().next();
+                    Log.d(TAG, "key: " + user.getKey());
+                    getUserWishlist(user.getKey());
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+    private void getUserWishlist(String userKey){
+
+        mDatabase.child("userWishlist/" + userKey + "/wishlist").addListenerForSingleValueEvent(
+                new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChildren()){
+                    Iterator<DataSnapshot> wishListIterator = dataSnapshot.getChildren().iterator();
+//                    Log.d(TAG, wishListIterator.next().getKey());
+
+                    while (wishListIterator.hasNext()){
+                        DataSnapshot currentItem = wishListIterator.next();
+
+                        String itemName = currentItem.child("item_name").getValue().toString();
+                        String itemLink = currentItem.child("item_link").getValue().toString();
+                        String item_image_url = currentItem.child("item_image_url").getValue().toString();
+                        double itemPrice = Double.valueOf(currentItem.child("item_price").getValue().toString());
+
+                        DisplayItem itemToDisplay = new DisplayItem(itemName, itemPrice, itemLink, item_image_url);
+                        wishListArrayList.add(itemToDisplay);
+                    }
+
+
+                    for (DisplayItem item1 : wishListArrayList){
+                        Log.d(TAG, item1.toString());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
